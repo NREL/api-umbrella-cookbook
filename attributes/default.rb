40c606695cc63f8c9976e77aafb12b6c5a0e9094
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-26 20:08:20 -0700

    Add log rotation for gatekeeper log file.

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: api-umbrella
+# Attributes:: api-umbrella
+#
+# Copyright 2013, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node.default[:api_umbrella][:gatekeeper][:logrotate_paths] = []
--- recipes/router.rb
+++ recipes/router.rb
@@ -7,6 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe "logrotate"
+include_recipe "sudo"
+
 user "api-umbrella-gatekeeper" do
   system true
   shell "/bin/false"
@@ -22,4 +25,13 @@ end
 
 sudo "api_umbrella" do
   template "sudo.erb"
+end
+
+logrotate_app "api_umbrella_gatekeeper" do
+  path node[:api_umbrella][:gatekeeper][:logrotate_paths]
+  frequency "daily"
+  rotate node[:supervisor][:logrotate][:rotate]
+  create "644 api-umbrella-gatekeeper api-umbrella-gatekeeper"
+  options %w(missingok compress delaycompress copytruncate notifempty)
+  sharedscripts true
 end
