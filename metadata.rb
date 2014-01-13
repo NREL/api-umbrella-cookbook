0d5eabff250efe71e529683a992459ce42a5bf64
Nick Muerdter <nick.muerdter@nrel.gov>
2014-01-12 22:11:54 -0700

    Bump to v0.2.1.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nick.muerdter@nrel.gov'
 license          'All rights reserved'
 description      'Installs/Configures api-umbrella'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'acl', '~> 0.1.0'
 depends 'bundler', '~> 0.1.4'
