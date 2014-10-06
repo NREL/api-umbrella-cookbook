9f09c0b7fca9d08a029b282ffcdd72b3dd9be7be
Nick Muerdter <nick.muerdter@nrel.gov>
2014-10-06 00:07:33 -0600

    New package release.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
+default[:api_umbrella][:version] = "0.6.0"
+default[:api_umbrella][:package_checksum] = "63e1824f6142c73d11fe677e64fe2c1f7065767694591a75a0ddd4a465e190f3"
+
 default[:api_umbrella][:config][:app_env] = "production"
 default[:api_umbrella][:config][:services] = [
   "general_db",
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,12 +9,12 @@
 
 require "yaml"
 
-package_name = "api-umbrella-0.6.0-1.el6.x86_64.rpm"
+package_name = "api-umbrella-#{node[:api_umbrella][:version]}-1.el6.x86_64.rpm"
 package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
 
 remote_file(package_local_path) do
   source "https://developer.nrel.gov/downloads/api-umbrella/#{package_name}"
-  checksum "c4173e5beb3e65d08888a724b7a360b09e70753acbd79342f60940044d2ecbe8"
+  checksum node[:api_umbrella][:package_checksum]
 end
 
 package "api-umbrella" do
