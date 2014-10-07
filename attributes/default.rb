65a582d63272a7495e00a3eb0dbc8625eb22ac54
Nick Muerdter <nick.muerdter@nrel.gov>
2014-10-07 10:39:13 -0600

    Update checksum for new testing package.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@
 #
 
 default[:api_umbrella][:version] = "0.6.0"
-default[:api_umbrella][:package_checksum] = "6351157c8ce713c0acdf50a8b610507894db3a790c394b6565e920dfa33dbfba"
+default[:api_umbrella][:package_checksum] = "ba6b91eb8fefb866125d186db283d95c490e2e4a5b46f4fff0b0b0e5ea445c69"
 
 default[:api_umbrella][:config][:services] = [
   "general_db",
