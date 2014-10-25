d54df347ceed0a0af381e2e4aecd88e6bb5bd517
Nick Muerdter <nick.muerdter@nrel.gov>
2014-10-25 10:12:37 -0600

    Add recipe to automate building the omnibus package via test-kitchen

---
+++ recipes/omnibus_build.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: api-umbrella
+# Recipe:: omnibus_build
+#
+# Copyright 2014, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "bundle install" do
+  cwd "/home/vagrant/api-umbrella"
+end
+
+execute "bin/omnibus build api-umbrella -l info" do
+  cwd "/home/vagrant/api-umbrella"
+end
