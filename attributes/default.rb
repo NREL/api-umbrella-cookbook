50aaba49099365c904e789018fc1d266db36930e
Nick Muerdter <nick.muerdter@nrel.gov>
2014-10-26 23:55:44 -0600

    Begin getting ready for the per-platform packages.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,44 @@
 #
 
 default[:api_umbrella][:version] = "0.6.0"
-default[:api_umbrella][:package_checksum] = "6729e74c7b6c89870901ca333d675beac0bb31a6abda944f8587ad8fc2fa5ad3"
+
+package_path = nil
+package_checksum = nil
+
+case node[:platform_family]
+when "rhel"
+  case(node[:platform_version].to_i)
+  when 6
+    package_path = "el/6/api-umbrella-#{node[:api_umbrella][:version]}-1.el6.x86_64.rpm"
+    package_checksum = "6729e74c7b6c89870901ca333d675beac0bb31a6abda944f8587ad8fc2fa5ad3"
+  when 7
+    package_path = "el/7/api-umbrella-#{node[:api_umbrella][:version]}-1.el7.x86_64.rpm"
+    package_checksum = "6729e74c7b6c89870901ca333d675beac0bb31a6abda944f8587ad8fc2fa5ad3"
+  end
+when "debian"
+  case(node[:platform])
+  when "debian"
+    case(node[:platform_version].to_i)
+    when 7
+      package_path = "debian/7/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
+      package_checksum = "6729e74c7b6c89870901ca333d675beac0bb31a6abda944f8587ad8fc2fa5ad3"
+    end
+  when "ubuntu"
+    case(node[:platform_version])
+    when "14.04"
+      package_path = "ubuntu/14.04/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
+      package_checksum = "6729e74c7b6c89870901ca333d675beac0bb31a6abda944f8587ad8fc2fa5ad3"
+    when "12.04"
+      package_path = "ubuntu/12.04/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
+      package_checksum = "6729e74c7b6c89870901ca333d675beac0bb31a6abda944f8587ad8fc2fa5ad3"
+    end
+  end
+end
+
+if(package_path && package_checksum)
+  default[:api_umbrella][:package_path] = "https://developer.nrel.gov/downloads/api-umbrella/#{package_path}"
+  default[:api_umbrella][:package_checksum] = package_checksum
+end
 
 default[:api_umbrella][:config][:services] = [
   "general_db",
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,18 +9,43 @@
 
 require "yaml"
 
-package_name = "api-umbrella-#{node[:api_umbrella][:version]}-1.el6.x86_64.rpm"
+if(!node[:api_umbrella][:package_url] || !node[:api_umbrella][:package_checksum])
+  raise "Unsupported platform or version: #{node[:platform]} #{node[:platform_version]}"
+end
+
+package_name = ::File.basename(node[:api_umbrella][:package_url])
 package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
 
-remote_file(package_local_path) do
-  source "https://developer.nrel.gov/downloads/api-umbrella/#{package_name}"
+remote_file "api-umbrella-package" do
+  path package_local_path
+  source node[:api_umbrella][:package_url]
   checksum node[:api_umbrella][:package_checksum]
+  only_if do
+    if(::File.exists?("/opt/api-umbrella/bin/api-umbrella"))
+      version = `/opt/api-umbrella/bin/api-umbrella --version`.strip
+      (version != "version #{node[:api_umbrella][:version]}")
+    else
+      true
+    end
+  end
 end
 
 package "api-umbrella" do
   source package_local_path
+  only_if { ::File.exists?(package_local_path) }
+end
+
+file "api-umbrella-package-cleanup" do
+  path package_local_path
+  action :delete
 end
 
 template "/etc/api-umbrella/api-umbrella.yml" do
   source "api-umbrella.yml.erb"
+  notifies :reload, "service[api-umbrella]"
+end
+
+service "api-umbrella" do
+  supports :restart => true, :status => true, :reload => true
+  action [:enable, :start]
 end
