5a7c98b6eda291bd27d346c856456d5f42b3fc0e
Nick Muerdter <nick.muerdter@nrel.gov>
2015-02-08 23:23:37 -0700

    Update for v0.7.0 package release.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:api_umbrella][:version] = "0.6.0"
+default[:api_umbrella][:version] = "0.7.0"
 
 package_path = nil
 package_checksum = nil
@@ -17,10 +17,10 @@ when "rhel"
   case(node[:platform_version].to_i)
   when 6
     package_path = "el/6/api-umbrella-#{node[:api_umbrella][:version]}-1.el6.x86_64.rpm"
-    package_checksum = "58efa3e8285871270eed5cd754f130bb51ba2d002a62a534b0b281d4bfd63ff8"
+    package_checksum = "00b54462ba7e83fca65bd380e3cd1d9364d00bcfb19ad313e5dc22a7c708bdf6"
   when 7
     package_path = "el/7/api-umbrella-#{node[:api_umbrella][:version]}-1.el7.x86_64.rpm"
-    package_checksum = "c04ac8b79ece518fbb7128d5d3e8ec1ca2b886c3812f3bd4f3125b00c37d5ff9"
+    package_checksum = "4c78a83e0b2f746a107373cd958b6ae49be8bff34dad947e4201f21c571d6828"
   end
 when "debian"
   case(node[:platform])
@@ -28,16 +28,16 @@ when "debian"
     case(node[:platform_version].to_i)
     when 7
       package_path = "debian/7/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
-      package_checksum = "4ceafe9554232c81da7cb993764e1c3a97adafb80d31d6f6e06332bd40372e67"
+      package_checksum = "bd4f9d71a2015ed564e5f27948a3bf932dd398dd2983cd2c747103d1bca545a7"
     end
   when "ubuntu"
     case(node[:platform_version])
     when "14.04"
       package_path = "ubuntu/14.04/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
-      package_checksum = "49cae3321ab0631e38cbc8f216e165bb229c86a95c52ae6a4ebfbb44a2359006"
+      package_checksum = "f0a926874223d38693e901ecba750dec9f4253a350afe97623a9cf6b948899c5"
     when "12.04"
       package_path = "ubuntu/12.04/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
-      package_checksum = "b82f0a648ed977ede2dfdc977103d27e530422ecf6fe3252e95dd385c5635384"
+      package_checksum = "56752c704761c53d56385c48d340f0d58e8f1a7610941518af58664abba6fe83"
     end
   end
 end
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nick.muerdter@nrel.gov'
 license          'All rights reserved'
 description      'Installs/Configures api-umbrella'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.6.0'
+version          '0.7.0'
 
 # For the "development" recipe
 depends "aws"
