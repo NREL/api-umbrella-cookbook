caf287902335e99a90312c7d11069c4727c4f620
Nick Muerdter <nick.muerdter@nrel.gov>
2015-04-26 19:11:04 -0600

    Update downloads to v0.8.0 packages.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:api_umbrella][:version] = "0.7.1"
+default[:api_umbrella][:version] = "0.8.0"
 
 package_path = nil
 package_checksum = nil
@@ -17,10 +17,10 @@ when "rhel"
   case(node[:platform_version].to_i)
   when 6
     package_path = "el/6/api-umbrella-#{node[:api_umbrella][:version]}-1.el6.x86_64.rpm"
-    package_checksum = "262a23fbcfd790daf715616a551f28712075e357e82a1da5b105b388b6f4ca33"
+    package_checksum = "47b1427465d971b99e87fb6220abea8399184701f19b989574928614b5234c08"
   when 7
     package_path = "el/7/api-umbrella-#{node[:api_umbrella][:version]}-1.el7.x86_64.rpm"
-    package_checksum = "fe25bc62b3e44273cd515fb8107864a6388803574e44fe145f1c2b6bf747fbea"
+    package_checksum = "6dc70c2498dba51026af99c510a89bb2f41fa8ec5a83afcf1b3be7962a1eadfd"
   end
 when "debian"
   case(node[:platform])
@@ -28,16 +28,16 @@ when "debian"
     case(node[:platform_version].to_i)
     when 7
       package_path = "debian/7/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
-      package_checksum = "dfb4a05df16329ebe2b5fb109889f9c99881283413084985567ac08245b0d019"
+      package_checksum = "09a9ce43db90bc0cc1a5c53a4ab1eaf1333a27354c5ffd3137880d4678b8a45d"
     end
   when "ubuntu"
     case(node[:platform_version])
     when "14.04"
       package_path = "ubuntu/14.04/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
-      package_checksum = "f1807a77ba6448b47d4ee86eba94ab135b4900a666f04552a334ff3c0a44a1d2"
+      package_checksum = "d295a8cba205df0100784e4b2617bb7245c30eeb5b2dc8e056cbe9e408ab23da"
     when "12.04"
       package_path = "ubuntu/12.04/api-umbrella-#{node[:api_umbrella][:version]}-1_amd64.deb"
-      package_checksum = "2291abae0105c110637e7b7af3d83f45b8aa45d914125316c9fb35e2c0b64946"
+      package_checksum = "503dfd80f3d7d46fe45a428cecfef1b8b84d783f763af00658d33eb28a1ba645"
     end
   end
 end
