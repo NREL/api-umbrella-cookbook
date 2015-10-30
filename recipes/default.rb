68b6efa45bd28be3bbc31c90198824dbccdea11b
Nick Muerdter <nick.muerdter@nrel.gov>
2015-10-30 00:49:02 -0600

    Experimenting with new yum repo installation.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,45 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 
-if(!node[:api_umbrella][:package_url] || !node[:api_umbrella][:package_checksum])
-  raise "Unsupported platform or version (platform: #{node[:platform].inspect}, platform_family: #{node[:platform_family].inspect}, platform_version: #{node[:platform_version].inspect})"
+yum_repository "api-umbrella" do
+  description "API Umbrella"
+  baseurl "https://dl.bintray.com/nrel/api-umbrella-el6"
+  gpgcheck false
+  gpgkey "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"
+  enabled true
+  action :create
 end
 
-package_name = ::File.basename(node[:api_umbrella][:package_url])
-package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
-
-remote_file "api-umbrella-package" do
-  path package_local_path
-  source node[:api_umbrella][:package_url]
-  checksum node[:api_umbrella][:package_checksum]
-  only_if do
-    if(::File.exists?("/opt/api-umbrella/bin/api-umbrella"))
-      version = `/opt/api-umbrella/bin/api-umbrella --version`.strip
-      (version != "version #{node[:api_umbrella][:version]}")
-    else
-      true
-    end
-  end
-end
-
-if(node[:platform_family] == "debian")
-  # dpkg doesn't install dependencies. We should look into a real apt repo.
-  package "gcc"
-end
-
-package "api-umbrella" do
-  source package_local_path
-  if(node[:platform_family] == "debian")
-    provider Chef::Provider::Package::Dpkg
-  end
-
-  only_if { ::File.exists?(package_local_path) }
-end
-
-file "api-umbrella-package-cleanup" do
-  path package_local_path
-  action :delete
-end
+package "api-umbrella"
 
 require "yaml"
 template "/etc/api-umbrella/api-umbrella.yml" do
@@ -53,9 +24,7 @@ template "/etc/api-umbrella/api-umbrella.yml" do
   notifies :reload, "service[api-umbrella]"
 end
 
-unless(node.recipe?("api-umbrella::development"))
-  service "api-umbrella" do
-    supports :restart => true, :status => true, :reload => true
-    action [:enable, :start]
-  end
+service "api-umbrella" do
+  supports :restart => true, :status => true, :reload => true
+  action [:enable, :start]
 end
