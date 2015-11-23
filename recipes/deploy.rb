503ac21915c884d348d67f6744715795a342f672
Nick Muerdter <nick.muerdter@nrel.gov>
2015-11-23 16:28:12 -0700

    Add zlib-devel for performing deploys with nokogiri updates.

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,2 +1,12 @@
 # For building gems when deploying from master.
 include_recipe "build-essential"
+
+# Nokogiri updates require zlib-devel as well.
+case(node["platform_family"])
+when "rhel", "fedora", "suse"
+  package "zlib-devel"
+when "arch"
+  package "zlib"
+else
+  package "zlib1g-dev"
+end
