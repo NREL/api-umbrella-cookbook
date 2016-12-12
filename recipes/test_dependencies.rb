a1f18eab80650b9557c5f39e47521656ec1791d0
Nick Muerdter <nick.muerdter@nrel.gov>
2016-12-11 20:16:03 -0700

    Shifting test package dependencies into api-umbrella repo script.
    These are now declared in the package_dependencies.sh script inside the
    api-umbrella repo.

--- recipes/test_dependencies.rb
+++ recipes/test_dependencies.rb
@@ -1,19 +1,2 @@
 # For python-virtualenv.
 include_recipe "yum-epel"
-
-packages = [
-  "expat-devel",
-  "libcurl-devel",
-
-  # For installing the mongo-orchestration test dependency.
-  "python-virtualenv",
-
-  # For checking for file descriptor leaks during the tests.
-  "lsof",
-]
-
-packages.each do |package_name|
-  package(package_name) do
-    action :install
-  end
-end
